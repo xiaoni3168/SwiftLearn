5c5c3ec673bd20c9c7ce2f51fab5e2da7a9a5fbb
Ani <nirui3168@163.com>
2015-10-30 00:41:21 +0800

    something need to learn

--- SwiftPlayground.playground/Contents.swift
+++ SwiftPlayground.playground/Contents.swift
@@ -337,3 +337,22 @@ func repeatItem<Item>(item: Item, numberOfTime: Int) -> [Item] {
     return result
 }
 repeatItem(7, numberOfTime: 4)
+
+enum OptionalValue<Wrapped> {
+    case None
+    case Some(Wrapped)
+}
+var possibleInteger: OptionalValue<Int> = .None
+possibleInteger = .Some(100)
+
+func anyCommonElements <T: SequenceType, U: SequenceType where T.Generator.Element: Equatable, T.Generator.Element == U.Generator.Element> (lhs: T, _ rhs: U) -> Bool {
+    for lhsItem in lhs {
+        for rhsItem in rhs {
+            if lhsItem == rhsItem {
+                return true
+            }
+        }
+    }
+    return false
+}
+anyCommonElements([1, 2, 3], [3])
